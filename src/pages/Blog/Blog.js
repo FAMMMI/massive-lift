5740ed78a04f8af135d1a54bc12a82abc01c72e5
FAMMMI <naimfami@gmail.com>
2022-04-18 14:13:05 +0600

    notfound page and blog page designed

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import './Blog.css'
 
 const Blog = () => {
     return (
-        <div className='p-3'>
+        <div className='p-3 blog-container'>
             <div className='mb-2'>
-                <h3>1. Authorization vs Authentication</h3>
+                <h3 className='text-center'>1. Authorization vs Authentication</h3>
                 <p><b>Answer:</b> Authentication prefers to the process of verifying who someone is, whereas Authorization is the process of verifying what specific applications, files, and data a user has access to. <br /> Authentication works through passwords, one-time pins, biometric information, and other information provided or entered by the user whereas Authorization works through settings that are implemented and maintained by the organization. <br />
                     Authentication is visible to and partially changeable by the user but Authorization is not visible to or changeable by the user. </p>
             </div>
+            <hr />
             <div>
-                <h3>2. Why are you using firebase? What are the options do you have to implement authentication ?</h3>
-                <p><b>Answer:</b> Firebase manages all data real-time in the database. So, the exchange of data from the database is easy and quick. <br /> There are various authentication types:
-                    *</p>
+                <h3 className='text-center'>2. Why are you using firebase? What are the options do you have to implement authentication ?</h3>
+                <p><b>Answer:</b> Firebase manages all data real-time in the database. So, the exchange of data from the database is easy and quick. <br /> There are various authentication types: <br />
+                    *STYTCH. <br />
+                    *Ory. <br />
+                    *Supabase. <br />
+                    *Okta. <br />
+                    *Keycloak.
+                </p>
             </div>
+            <hr />
             <div>
-                <h3>3. What other services does firebase provide other than authentication?</h3>
+                <h3 className='text-center'>3. What other services does firebase provide other than authentication?</h3>
                 <p><b>Answer:</b> Firebase provides <b>Cloud storage</b> as well as <b>cloud Firestore </b>. It also provide <b>Realtime Database</b> . It also supports its user with <b>Firebase Hosting</b> .  </p>
             </div>
         </div>
--- src/pages/Shared/NotFound/NotFound.js
+++ src/pages/Shared/NotFound/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div className='my-auto'>
+        <div className='my-auto not-found-container'>
             <h1 className='text-danger text-center'>You have come to a wrong directed page.  </h1>
-            <h2 className='text-danger text-center'>Please GO Back to Home .</h2>
+            <h2 className='text-danger text-center'>Please GO Back to
+                <Link to="/home" className='home-text btn ms-2'>Home</Link></h2>
+            <img src="https://www.fixrunner.com/wp-content/uploads/2018/08/fix-wordpress-error-404-page-not-found.jpg" alt="" />
         </div>
     );
 };
