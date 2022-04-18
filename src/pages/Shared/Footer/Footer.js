1843d6c71cbdd16c2b668422eb3e62df1571593a
FAMMMI <naimfami@gmail.com>
2022-04-18 17:49:59 +0600

    slightly configured for mobile device

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
         <footer className='text-center mt-5 footer-container pb-0'>
 
             <div className='footer-detail'>
-                <div className='text-white'>
+                <div className='text-white contact-detail'>
                     <p className='mb-0'>
                         <img style={{ width: "60px", margin: "5px" }} src="https://i.ibb.co/XzP57BP/logo-2.png" alt="" />
                         <b>Massive Lift-Up</b>
@@ -21,14 +21,14 @@ const Footer = () => {
                     <p className='pb-1 mb-0'>Phone: 01515681709</p>
                     <p className='pb-1 mb-0'>Email: <a className='email text-white' href="https://www.naimfami@gmail.com">naimfami@gmail.com</a> </p>
                 </div>
-                <div className='d-flex align-items-center justify-content-center gap-3 p-4'>
+                <div className='d-flex align-items-center justify-content-center gap-3 p-0 '>
                     <p className='text-white m-1'><b>Our Social handle :</b></p>
                     <a href="https://www.facebook.com"><img width={30} src={fblogo} alt="" /></a>
                     <a href="https://www.github.com"><img src={gitlogo} alt="" /></a>
                     <a href="https://www.google.com"><img src={googlelogo} alt="" /></a>
                 </div>
             </div>
-            <p className='mb-0 pb-3 '><small className='text-white'>Massive Lift-Up -- copyright © {year} </small></p>
+            <p className='mb-0 pb-3 pt-4'><small className='text-white'>Massive Lift-Up -- copyright © {year} </small></p>
         </footer>
     );
 };
